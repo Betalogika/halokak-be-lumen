e96bf162b1f7c108896b84496fc09d247fa5926c
Muhammad Syamsul Marif F1B118038 <syamsulmar_f1b118038@student.uho.ac.id>
2023-09-29 16:05:24 +0800

    config allow cors

--- config/cors.php
+++ config/cors.php
@@ -5,37 +5,37 @@ return [
      * You can enable CORS for 1 or multiple paths.
      * Example: ['api/*']
      */
-    'paths' => [],
+    'paths' => ['/*',],
 
     /*
-    * Matches the request method. `['*']` allows all methods.
+    * Matches the request method. `[*]` allows all methods.
     */
     'allowed_methods' => ['*'],
 
     /*
-     * Matches the request origin. `['*']` allows all origins. Wildcards can be used, eg `*.mydomain.com`
+     * Matches the request origin. `[*]` allows all origins.
      */
     'allowed_origins' => ['*'],
 
     /*
-     * Patterns that can be used with `preg_match` to match the origin.
+     * Matches the request origin with, similar to `Request::is()`
      */
     'allowed_origins_patterns' => [],
 
     /*
-     * Sets the Access-Control-Allow-Headers response header. `['*']` allows all headers.
+     * Sets the Access-Control-Allow-Headers response header. `[*]` allows all headers.
      */
     'allowed_headers' => ['*'],
 
     /*
-     * Sets the Access-Control-Expose-Headers response header with these headers.
+     * Sets the Access-Control-Expose-Headers response header.
      */
-    'exposed_headers' => [],
+    'exposed_headers' => false,
 
     /*
-     * Sets the Access-Control-Max-Age response header when > 0.
+     * Sets the Access-Control-Max-Age response header.
      */
-    'max_age' => 0,
+    'max_age' => false,
 
     /*
      * Sets the Access-Control-Allow-Credentials header.
