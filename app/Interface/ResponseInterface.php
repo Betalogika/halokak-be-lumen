cce9e71df5f7a45358ae9cb306738efc852f010b
Xamppsul <syamsulmar_f1b118038@student.uho.ac.id>
2023-12-13 19:48:10 +0800

    fix response error format

--- app/Interface/ResponseInterface.php
+++ app/Interface/ResponseInterface.php
@@ -6,6 +6,6 @@ namespace App\Interface;
 interface ResponseInterface
 {
     public function ok($data, String $message = 'Successfully Data', int $statusCode = 200);
-    public function error(String $message = 'Errors Data', int $statusCode = 422, String $data = '');
+    public function error(String $message = 'Errors Data', int $statusCode = 422);
     public function customError($data);
 }
